7f16b7fb69b053544b00af99dd8f0959e0a95413
28andrew <andrewtran312@gmail.com>
2022-04-05 23:52:40 -0500

    Four ball tweaks in simulator

--- src/main/java/frc/robot/commands/auto/FourBallAuto4.java
+++ src/main/java/frc/robot/commands/auto/FourBallAuto4.java
@@ -9,6 +9,7 @@ import edu.wpi.first.math.trajectory.constraint.EllipticalRegionConstraint;
 import edu.wpi.first.math.trajectory.constraint.MaxVelocityConstraint;
 import edu.wpi.first.math.util.Units;
 import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.ParallelDeadlineGroup;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
@@ -28,7 +29,7 @@ public class FourBallAuto4 extends SequentialCommandGroup {
     //private static final String THIRD_TRAJECTORY = "4_A2"; // Reverse to turn around
     //private static final String FOURTH_TRAJECTORY = "4_A3"; // Forward to shooting position
     private static final double SLOW_DOWN_RADIUS = Units.feetToMeters(2.0); // Radius around balls to slow down
-    private static final double SLOW_DOWN_VELOCITY = 3.0;
+    private static final double SLOW_DOWN_VELOCITY = 2.0;
 
     private static final Pose2d START_POSE = new Pose2d(7.64, 1.986, new Rotation2d(4.712));
     private static final Translation2d FIRST_PICKUP_TRANSLATION = new Translation2d(7.63, 1.502);
@@ -37,23 +38,28 @@ public class FourBallAuto4 extends SequentialCommandGroup {
     private static final Pose2d INTERMEDIATE_POSE = new Pose2d(8.37,1.93, new Rotation2d(Math.PI));
     private static final Pose2d SHOOTING_POSE = new Pose2d(7.82, 2.67, new Rotation2d(1.1906147));
     // Intermediate pose 2
-    private static final Pose2d INTERMEDIATE_TWO_POSE = new Pose2d(7.61, 1.9, new Rotation2d(Math.PI/2));
-    private static final Pose2d INTERMEDIATE_THREE_POSE = new Pose2d(7.72, 1.57, new Rotation2d(2.7));
+    private static final Pose2d INTERMEDIATE_TWO_POSE = new Pose2d(7.61, 1.7, new Rotation2d(Math.PI/2));
+    private static final Pose2d INTERMEDIATE_THREE_POSE = new Pose2d(8.11, 1.77, new Rotation2d(2.9));
+    // Shooting position 2 (a little more left)
+    private static final Pose2d SHOOTING_POSE_2 = new Pose2d(7.66, 2.82, new Rotation2d(1.1906));
 
-    // https://www.desmos.com/calculator/iouvkxkemq
+    // https://www.desmos.com/calculator/i7bwshyg4t
 
     public FourBallAuto4(DriveTrain driveTrain, CargoManipulator cargoManipulator, Arm arm, Vision vision) {
         //var forwardConfig = TrajectoryUtil.createConfig(MAX_VELOCITY, MAX_ACCEL);
-        var forwardWithBallsConfig = TrajectoryUtil.createConfig(3.0, 1.0);
+        var forwardWithBallsConfig = TrajectoryUtil.createConfig(3.0, 1.5);
         // Add slow down near balls
         List.of(Cargo.B, Cargo.TERMINAL).forEach(cargo ->
                 forwardWithBallsConfig.addConstraint(new EllipticalRegionConstraint(cargo.getPosition(),
                         SLOW_DOWN_RADIUS * 2, SLOW_DOWN_RADIUS * 2, new Rotation2d(),
                         new MaxVelocityConstraint(SLOW_DOWN_VELOCITY))));
+        forwardWithBallsConfig.setEndVelocity(1.75); // don't need to slow down when picking up last ball
 
         //var reverseConfig = TrajectoryUtil.createConfig(MAX_VELOCITY, MAX_ACCEL, true);
 
-        Trajectory firstTrajectory = TrajectoryUtil.generateTrajectory(START_POSE, Cargo.A.getPickupPose(FIRST_PICKUP_TRANSLATION), TrajectoryUtil.createConfig(2.5, 1.0));
+        var firstConfig = TrajectoryUtil.createConfig(2.5, 1.0);
+        firstConfig.setEndVelocity(DriveTrain.convertPercentToVelocity(AutoBallPickupCommand.DRIVE_SPEED)); // make first movement end velocity be same as intake movement speed
+        Trajectory firstTrajectory = TrajectoryUtil.generateTrajectory(START_POSE, Cargo.A.getPickupPose(FIRST_PICKUP_TRANSLATION), firstConfig);
 
         Trajectory pickupTrajectory = TrajectoryUtil.generateTrajectory(SECOND_TRAJECTORY, forwardWithBallsConfig);
 
@@ -83,13 +89,13 @@ public class FourBallAuto4 extends SequentialCommandGroup {
                 new SequentialCommandGroup(
                         // Drive backwards to BEFORE shoot position
                         new LazyRamseteCommand(driveTrain, () -> {
-                            var config1 = TrajectoryUtil.createConfig(4.0, 2.75, true);
+                            var config1 = TrajectoryUtil.createConfig(4.0, 3.0, true);
                             //config1.setEndVelocity(0.5);
                             return TrajectoryUtil.generateTrajectory(driveTrain.getPose(), INTERMEDIATE_POSE, config1);
                         }, false),
                         // Drive forwards to shoot position
                         new LazyRamseteCommand(driveTrain, () -> {
-                            var config1 = TrajectoryUtil.createConfig(4.0, 2.75);
+                            var config1 = TrajectoryUtil.createConfig(4.0, 3.0);
                             //config1.setEndVelocity(1.5);
                             return TrajectoryUtil.generateTrajectory(driveTrain.getPose(), SHOOTING_POSE, config1);
                         })
@@ -99,24 +105,21 @@ public class FourBallAuto4 extends SequentialCommandGroup {
             ),
             // Shoot
             new CargoShootCommand(cargoManipulator).withTimeout(0.75),
-            // Drive back to intermediate 2 w/ arm down
+            // Drive back to intermediate 2 w/ arm down & pickup
             new ParallelDeadlineGroup(
-                new LazyRamseteCommand(driveTrain, () -> {
-                    var config1 = TrajectoryUtil.createConfig(4.0, 3.0, true);
-                    //config1.setEndVelocity(1.0);
-                    return TrajectoryUtil.generateTrajectory(driveTrain.getPose(), INTERMEDIATE_TWO_POSE, config1);
-                }),
-                new CargoIntakeCommand(cargoManipulator),
+                new SequentialCommandGroup(
+                    new LazyRamseteCommand(driveTrain, () -> {
+                        var config1 = TrajectoryUtil.createConfig(4.0, 3.0, true);
+                        //config1.setEndVelocity(1.0);
+                        return TrajectoryUtil.generateTrajectory(driveTrain.getPose(), INTERMEDIATE_TWO_POSE, config1);
+                    }),
+                    TrajectoryUtil.createCommand(pickupTrajectory, driveTrain)
+                ),
+                new CargoIntakeCommand(cargoManipulator, 0.75),
                 new SequentialCommandGroup(
                     new WaitCommand(0.5),
                     new ArmMoveToCommand(arm, Arm.ArmPosition.DOWN)
                 )
-            ),
-            // Drive path to pickup balls
-            new ParallelDeadlineGroup(
-                TrajectoryUtil.createCommand(pickupTrajectory, driveTrain),
-                new CargoIntakeCommand(cargoManipulator, 0.75),
-                new ArmHoldPositionCommand(arm, Arm.ArmPosition.DOWN)
             ),
             // Drive back to intermediate 3
             new ParallelDeadlineGroup(
@@ -124,7 +127,7 @@ public class FourBallAuto4 extends SequentialCommandGroup {
                     TrajectoryConfig config1 = TrajectoryUtil.createConfig(4.0, 2.5, true);
                     //config1.setEndVelocity(1.5);
                     return TrajectoryUtil.generateTrajectory(driveTrain.getPose(),
-                            List.of(new Translation2d(5.32,2.11),new Translation2d(6.32,2.11)),
+                            List.of(new Translation2d(5.32,1.9)),
                             INTERMEDIATE_THREE_POSE, config1);
                 }),
                 new CargoIntakeCommand(cargoManipulator),
@@ -133,15 +136,15 @@ public class FourBallAuto4 extends SequentialCommandGroup {
             // Drive back & shoot
             new ParallelDeadlineGroup(
                     new LazyRamseteCommand(driveTrain, () -> {
-                        TrajectoryConfig config1 = TrajectoryUtil.createConfig(2.75, 1.5);
-                        config1.setEndVelocity(0.5);
-                        return TrajectoryUtil.generateTrajectory(driveTrain.getPose(), SHOOTING_POSE, config1);
+                        TrajectoryConfig config1 = TrajectoryUtil.createConfig(4.0, 1.5);
+                        config1.setEndVelocity(1.0);
+                        return TrajectoryUtil.generateTrajectory(driveTrain.getPose(), SHOOTING_POSE_2, config1);
                     }),
                 new CargoIntakeCommand(cargoManipulator),
                 new ArmHoldPositionCommand(arm, Arm.ArmPosition.UP)
             ),
-            // Shoot & print time at shoot,
-            new InstantCommand(() -> System.out.println("Shooting at " + timer.get())),
+            // Shoot & display time at shoot,
+            new InstantCommand(() -> SmartDashboard.putNumber("Auto Shoot Time", timer.get())),
             new CargoShootCommand(cargoManipulator)
         );
     }
